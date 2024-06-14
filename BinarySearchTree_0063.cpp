3519dc868966f4a15163244e441663f35cc41d36
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-06-14 01:48:07 -0700

    menambahkan loop pada postorder

--- BinarySearchTree_0063.cpp
+++ BinarySearchTree_0063.cpp
@@ -105,7 +105,17 @@ public:
 
 	void postorder(Node* ptr)
 	{
-
+		if (ROOT == NULL)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+			postorder(ptr->leftchild);
+			postorder(ptr->rightchild);
+			cout << ptr->info << " ";
+		}
 	}
 };
 
